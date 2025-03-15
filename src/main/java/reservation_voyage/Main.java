0806e3e1573d1006b8cdf05b9fc1dbff7b72e5e4
AbdellatifTensai <zacksab12@gmail.com>
2025-03-15 00:31:38 +0000

    first commit

---
+++ src/main/java/reservation_voyage/Main.java
@@ -0,0 +1,12 @@
+package reservation_voyage;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.Persistence;
+
+public class Main {
+
+    public static void main(String[] args) {
+        EntityManager em = Persistence.createEntityManagerFactory("ReservationVoyage").createEntityManager();
+    }
+
+}
---
+++ src/main/java/reservation_voyage/Metier/Voyage.java
@@ -0,0 +1,5 @@
+package reservation_voyage.DAO;
+
+public class Voyage {
+
+}
